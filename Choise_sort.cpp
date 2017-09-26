3a9a2a6d30e76f6774800ad03880da359c3b6aec
RaiMaN <mrkenyfull@gmail.com>
2017-09-26 17:29:38 +0300

    added Choise_sort

---
+++ Choise_sort.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <algorithm>
+#include <fstream>
+#define MAX_K 100
+using namespace std;
+int main() {
+	ifstream fin;
+	ofstream fout;
+	fin.open("in.txt");
+	fout.open("out.txt");
+	int a[MAX_K], N;
+	fin >> N;
+	for (int i = 0; i < N; i++) {
+		fin >> a[i];
+	}
+	int pos;
+    for (int i = 0; i < N; i++) {
+    	pos = i;
+    	for(int j = i + 1; j < N; j++) {
+    		if(a[pos] > a[j]) {
+    			pos = j;
+			}
+		}
+		swap(a[pos], a[i]);
+	}
+	for(int i = 0; i < N; i++) {
+		fout << a[i] << " ";
+	}
+	fout.close();
+	return 0;
+}
